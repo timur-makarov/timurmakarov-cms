867e3ed71af8809fec3004eaf35567533d6f2e73
Timur Makarov <public@timurmakarov.com>
2025-01-12 13:25:21 +0400

    chore: update strapi to version 5

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', "salt"),
   },
   transfer: {
     token: {
--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 4000),
   app: {
-    keys: env.array("APP_KEYS"),
+    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
   },
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
